c6903805a45aeffe64d7acb2706c9cd68659cdbd
lipine153 <258843855+lipine153@users.noreply.github.com>
2026-06-05 18:31:21 +0800

    let pinedoc output .txt,add searchRegex for tree func,use <locale> to output chinese

--- pinedoc.cpp
+++ pinedoc.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <windows.h>
 #include <string>
+#include <vector>
 #include <regex>
-#include <clocale>
+
+#include <locale>
 #define BUFSIZE MAX_PATH
 
 using namespace std;
@@ -10,42 +13,55 @@ using namespace std;
 wchar_t* BlackList[3] = {L".", L"..", L"desktop.ini"};
 wstring RootDir = L"D:\\document";
 wstring wildcard = L"\\*";
-wregex pdf(L"\\.pdf$");
 
+locale chineseLocale("chs");
 
 bool compareDir(wchar_t str[]);
 bool isDir(unsigned int dir);
-inline wstring toWideString(wchar_t* arr) {
+inline wstring toWideString(wchar_t* arr) {//将wchar_t* 变为wstring
     return wstring(arr);
 }
-void tree(wstring currentDir);
+void tree(wstring currentDir, wregex searchRegex);
 int wmain(int argc, wchar_t* argv[]){
-    setlocale(LC_ALL, "chs");
+    wcout.imbue(chineseLocale);
 
-    if (argc >= 2){
-        std::wcout << argv[1] << std::endl;
-    }else{
+    vector<wstring> vectorArg;
+    for (int i = 1; i < argc; i++){
+        vectorArg.emplace_back(toWideString(argv[i]));
+        wcout << vectorArg[i-1] << endl;
+    }//得到输入参数向量
+    if (argc == 1){
         std::cout << "No input args" << std::endl;
     }
 
-//**************************************************
-    tree(RootDir);
+/*******************************************************************/
+    wregex searchRegex(L"\\.pdf$");
+    tree(RootDir, searchRegex);
     return 0;
 }
 
-void tree(wstring currentDir){
+void tree(wstring currentDir, wregex searchRegex){//递归函数来深度遍历目录树
+    static wofstream outputDocList(L"docList.txt");
+
+    outputDocList.imbue(chineseLocale);
+    wcout.imbue(chineseLocale);
+    //绑定语言
+
     WIN32_FIND_DATAW dirData;
     HANDLE dirHandle = FindFirstFileW((currentDir + wildcard).c_str(), &dirData);
     do{
         if (compareDir(dirData.cFileName)){
-            continue;
+            continue;//黑名单检测
         }
-        if (regex_search(dirData.cFileName, pdf)){
-            wcout << (currentDir + toWideString(L"\\") + toWideString(dirData.cFileName)) << endl;
-            cout << dirData.dwFileAttributes << endl;
+        if (regex_search(dirData.cFileName, searchRegex)){
+            //文件路径写入docList
+            outputDocList << (currentDir + toWideString(L"\\") + toWideString(dirData.cFileName)) << endl;
+//            wcout << dirData.cFileName << endl;
         }
         if (isDir(dirData.dwFileAttributes)){
-            tree((currentDir + toWideString(L"\\") + toWideString(dirData.cFileName)));
+            //递归在这里
+            tree((currentDir + toWideString(L"\\") + toWideString(dirData.cFileName)), searchRegex);
+
         }
     }while(FindNextFileW(dirHandle, &dirData) != 0);
 }
@@ -58,7 +74,7 @@ bool compareDir(wchar_t str[]){//若在黑名单，返回true
     return false;
 }
 
-bool isDir(unsigned int dir){
+bool isDir(unsigned int dir){//判断WIN32_FIND_DATAW结构中的DWORD dwFileAttributes值的第四位，表示目录
     if ((dir & 0x10) == 0x10)
         return true;
     return false;
